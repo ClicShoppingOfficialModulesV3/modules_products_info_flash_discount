cc7cf2d514d4ab672ad76f0352443cc5df4b7bc1
ClicShopping <Yae8mr2o*>
2019-12-30 14:56:10 -0500

    version3.2113

--- sources/template/Default/modules/modules_products_info/content/products_info_flash_discount.php
+++ sources/template/Default/modules/modules_products_info/content/products_info_flash_discount.php
@@ -11,11 +11,9 @@
 
 use ClicShopping\OM\CLICSHOPPING;
 ?>
-<div class="<?php echo $text_position; ?> col-md-<?php echo $content_width; ?>">
+<div class="col-md-<?php echo $content_width; ?> <?php echo $text_position; ?>">
   <div class="separator"></div>
-  <div style="float: <?php echo MODULE_PRODUCTS_INFO_FLASH_DISCOUNT_POSITION; ?>;">
     <div class="moduleProductsInfoFlashDiscount">
       <span class="moduleProductsInfoFlashDiscount"><?php echo CLICSHOPPING::getDef('text_flash_discount') . '<br/>' . $products_flash_discount; ?></span>
-    </div>
   </div>
 </div>
